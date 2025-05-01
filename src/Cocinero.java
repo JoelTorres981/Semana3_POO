46f70b29c1f0fb74dfd38a90a552eea8e20c14f9
POO <joel.torres@epn.edu.ec>
2025-05-01 07:43:46 -0500

    Parte 1 finalizada

---
+++ src/Cocinero.java
@@ -0,0 +1,18 @@
+public class Cocinero {
+    String nombre, especialidad;
+    int experiencia;
+
+    public Cocinero(String nombre, String especialidad, int experiencia) {
+        this.nombre=nombre;
+        this.especialidad=especialidad;
+        this.experiencia=experiencia;
+    }
+
+    public void mostrarinfo(){
+        System.out.println("INFORMACION");
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Especialidad: "+especialidad);
+        System.out.println("Años de experiencia: "+experiencia);
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nombre, especialidad;
+        int experiencia;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese informacion");
+        System.out.println("Nombre: ");
+        nombre = scanner.nextLine();
+        System.out.println("Especialidad: ");
+        especialidad = scanner.nextLine();
+        System.out.println("Experiencia: ");
+        experiencia = scanner.nextInt();
+
+        Cocinero cocinero = new Cocinero(nombre,especialidad,experiencia);
+
+        cocinero.mostrarinfo();
     }
 }
