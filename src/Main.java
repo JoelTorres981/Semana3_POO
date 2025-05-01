0424c8c8196337923ae00e208f94968d9b412823
POO <joel.torres@epn.edu.ec>
2025-05-01 07:59:23 -0500

    Parte 1 finalizada 3 cocineros

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
 
         for (int i = 0; i < cocineros.length; i++) {
             cocineros[i].mostrarinfo();
-            System.out.println("");
+            System.out.println(" ");
         }
 
     }
